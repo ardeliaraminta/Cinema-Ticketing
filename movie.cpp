21f2faf32009d9ba48169d0331454cd9d02529be
kalyanrohan <sri.rohan@binus.ac.id>
2021-06-26 17:02:05 +0700

    fsafasfsaf

--- movie.cpp
+++ movie.cpp
@@ -1,3 +1,51 @@
+#include<iostream>
+#include<stdlib.h>
+#include<bits/stdc++.h>
+#include<list>
+#include<D:\CSFILES\COMSCI\C++\final project dsa\Cinema-Ticketing\indoXXI.cpp>
+
+using namespace std;
+
+class Movie:public indoXXI{
+    public:
+    string title;
+    string time;
+    string date;
+
+    Movie(string title_entry,string time_entry,string date_entry){
+        title=title_entry;
+        this->time=time_entry;
+        this->date=date_entry;
+    };
+
+    Movie(){
+
+    };
+
+    string getTitle();
+    string getTime();
+    string getDate();
+    void setTitle(string title);
+    void setTime(string time);
+    void setDate(string date);
+
+
+};
+
+string Movie::getTitle(){
+  return title;
+}
+
+string Movie::getTime(){
+  return time;
+}
+
+string Movie::getDate(){
+  return date;
+}
+
+void Movie::setTitle(string title){
+  this->title=title;
 }
 
 void Movie::setTime(string time){
